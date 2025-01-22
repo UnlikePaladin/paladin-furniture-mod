371ec1bff019c9b3f5f1d93b936da853a68017f3
UnlikePaladin <36827970+UnlikePaladin@users.noreply.github.com>
2025-01-22 17:35:49 -0600

    Turns out entities vanished on 1.17 too, fixed

--- common/src/main/java/com/unlikepaladin/pfm/runtime/ClientOverlaySetter.java
+++ common/src/main/java/com/unlikepaladin/pfm/runtime/ClientOverlaySetter.java
@@ -3,6 +3,7 @@ package com.unlikepaladin.pfm.runtime;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.unlikepaladin.pfm.client.screens.overlay.PFMGeneratingOverlay;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Util;
 
 public class ClientOverlaySetter {
@@ -14,12 +15,23 @@ public class ClientOverlaySetter {
 
     public static void updateScreen() {
         MinecraftClient client = MinecraftClient.getInstance();
+
+        MatrixStack matrixStack = RenderSystem.getModelViewStack();
+
+        matrixStack.push();
+        RenderSystem.applyModelViewMatrix();
         client.getFramebuffer().beginWrite(true);
         long i = Util.getMeasuringTimeNano();
         client.gameRenderer.render(1, i, false);
         client.getFramebuffer().endWrite();
+        matrixStack.pop();
+
+        matrixStack.push();
         RenderSystem.applyModelViewMatrix();
         client.getFramebuffer().draw(client.getWindow().getFramebufferWidth(), client.getWindow().getFramebufferHeight());
+        matrixStack.pop();
+
+        RenderSystem.applyModelViewMatrix();
         client.getWindow().swapBuffers();
     }
 
