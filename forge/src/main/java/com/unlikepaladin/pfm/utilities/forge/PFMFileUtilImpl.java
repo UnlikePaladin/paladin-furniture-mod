8b14ef90bcd375bc56294c7261b995a1e08d86fd
UnlikePaladin <36827970+UnlikePaladin@users.noreply.github.com>
2025-01-22 18:58:29 -0600

    Fix wrong translations for some mods like Axar Adapt

--- forge/src/main/java/com/unlikepaladin/pfm/utilities/forge/PFMFileUtilImpl.java
+++ forge/src/main/java/com/unlikepaladin/pfm/utilities/forge/PFMFileUtilImpl.java
@@ -1,13 +1,20 @@
 package com.unlikepaladin.pfm.utilities.forge;
 
 import com.unlikepaladin.pfm.utilities.PFMFileUtil;
+import net.minecraft.resource.ResourcePack;
 import net.minecraftforge.fml.loading.FMLPaths;
 
 import java.nio.file.Path;
+import java.util.Collections;
+import java.util.List;
 
 public class PFMFileUtilImpl {
     public static Path getGamePath() {
         return FMLPaths.GAMEDIR.relative().normalize();
+    }
+
+    public static List<ResourcePack> getSubPacks(ResourcePack pack) {
+        return Collections.singletonList(pack);
     }
 
     public static PFMFileUtil.ModLoader getModLoader() {
